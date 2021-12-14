b913fab22503f51fb8612e5730db428fa5633b27
robert_bao <ibaochen@gmail.com>
2021-12-13 21:09:29 -0500

    Create functional backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('weather/<str:city_name>',
+         views.get_weather_info,
+         name='weather info'),
+    path('admin/', admin.site.urls),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+
 # Create your views here.
+
+apikey = "FbHh01xGcDPd2YOkrGjAZxEUAz1SebiM"
+base_location_url = "https://dataservice.accuweather.com/locations/v1/cities/search?apikey="
+base_forecast_url = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/"
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the lucid air index.")
+
+
+# get the weather
+def get_weather_info(request, city_name):
+    location_url = base_location_url + apikey + "&q={}".format(city_name)
+    location_res = requests.get(location_url)
+    city_key = location_res.json()[0]['Key']
+
+    forecastUrl = base_forecast_url + city_key + "?apikey={}&details=true".format(
+        apikey)
+    forcast_res = requests.get(forecastUrl)
+    return HttpResponse(forcast_res)
