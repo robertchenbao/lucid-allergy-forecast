8ea6026ee500e5802a1e5bf11b51ed447597d4c9
robert_bao <ibaochen@gmail.com>
2021-12-13 21:12:39 -0500

    Apply backend security updates

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from decouple import config
 
 import requests
 
 # Create your views here.
 
-apikey = "FbHh01xGcDPd2YOkrGjAZxEUAz1SebiM"
+API_KEY = config('API_KEY', default='')
 base_location_url = "https://dataservice.accuweather.com/locations/v1/cities/search?apikey="
 base_forecast_url = "https://dataservice.accuweather.com/forecasts/v1/daily/5day/"
 
@@ -16,11 +17,11 @@ def index(request):
 
 # get the weather
 def get_weather_info(request, city_name):
-    location_url = base_location_url + apikey + "&q={}".format(city_name)
+    location_url = base_location_url + API_KEY + "&q={}".format(city_name)
     location_res = requests.get(location_url)
     city_key = location_res.json()[0]['Key']
 
     forecastUrl = base_forecast_url + city_key + "?apikey={}&details=true".format(
-        apikey)
+        API_KEY)
     forcast_res = requests.get(forecastUrl)
     return HttpResponse(forcast_res)
