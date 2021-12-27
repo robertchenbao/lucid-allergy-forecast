575d5f99b1efea93d0a846acce2d3681a178c48d
robert_bao <ibaochen@gmail.com>
2021-12-27 16:33:04 -0500

    Remove tailwind plugin list

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,15 +2,10 @@ module.exports = {
     content: [
         "./public/index.html",
         "./src/**/*.{js,jsx,ts,tsx}",
-        "./src/*.{js,jsx,ts,tsx}",
+        "./src/*.js",
     ],
     theme: {
         extend: {},
     },
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        cssnano: {},
-        // ...(NODE_ENV === "production" ? { cssnano: {} } : {}),
-    },
+    plugins: [],
 };
